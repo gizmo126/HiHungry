04d6c12028cc3217cdc09ec5a3e236be51040f28
gizmo126 <samghuang@gmail.com>
2018-03-12 17:11:23 -0500

    initial setup

--- app/connect.php
+++ app/connect.php
@@ -1,14 +1,9 @@
 
 <?php
-$mysql_hostname = "cpanel3.engr.illinois.edu";
-$mysql_user = "gizmohihungry_admin";
-$mysql_password = "password123";
-$mysql_database = "gizmohihungry_test";
-
-define('DBHOST', 'localhost');
-define('DBUSER', 'gizmohihungry_admin');
-define('DBPASS', 'password123');
-define('DBNAME', 'gizmohihungry_test');
+define('DBHOST', ''); // cpanel3.engr.illinois.edu
+define('DBUSER', ''); // gizmohihungry_admin
+define('DBPASS', ''); // password123
+define('DBNAME', ''); // gizmohihungry_test
 
 $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 if ( !$conn ) {
